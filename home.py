9e8ce00909d59f18f9329072373bb9747ff9fb7a
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-19 07:31:09 +0700

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ st.title("Python Talks Search Engine")
 # Connect to the Google Sheet
 sheet_id = "1mdvsYlPVQ0Tda35frF1zoznq9TFtOWe0dKkgWDJkiS0"
 sheet_name = "bibliograph"
-url = f"<https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}>"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url)
 
 # Show the dataframe (we'll delete this later)
