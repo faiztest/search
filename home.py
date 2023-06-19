8c24d3442dde593ba91b3ede8b3049a9da4ff277
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-19 08:47:18 +0700

    Update home.py

--- home.py
+++ home.py
@@ -34,14 +34,14 @@ search_opt = c3.multiselect(
 if type_for != format_options[0]:
         #res_df = df.loc[df['gmd_id'].isin(type_for)]
         res_df = df[df['gmd_id'].str.contains(type_for)]
-        st.write(res_df)
+        #st.write(res_df)
 
-#listmat = pd.DataFrame(columns=['biblio_id', 'url', 'gmd_id', 'title', 'author', 'year', 'callnum', 'full-text'])
-#for col in search_opt:
-#        mat = res_df[col].apply(lambda x: any(w in x for w in keyword_list))
-#        listmat.append(mat)
+listmat = pd.DataFrame(columns=['biblio_id', 'url', 'gmd_id', 'title', 'author', 'year', 'callnum', 'full-text'])
+for col in search_opt:
+        mat = res_df[col].apply(lambda x: any(w in x for w in keyword_list))
+        listmat.append(mat)
 
 
 
 # Show the dataframe (we'll delete this later)
-#st.write(listmat)
+st.write(listmat)
