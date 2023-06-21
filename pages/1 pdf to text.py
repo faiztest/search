876931a62d3d9ba3341ac170de5afb040a4d7a4a
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 11:41:05 +0700

    Update 1 pdf to text.py

--- pages/1 pdf to text.py
+++ pages/1 pdf to text.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import PyPDF2
 import pandas as pd
+import os
 
-#===config===
+# === config ===
 st.set_page_config(
-     page_title="Pencarian Terpadu",
-     page_icon="https://digilib.polteknuklir.ac.id/perpus/images/default/logo.png",
-     layout="wide"
+    page_title="Pencarian Terpadu",
+    page_icon="https://digilib.polteknuklir.ac.id/perpus/images/default/logo.png",
+    layout="wide"
 )
 
 def reset_data():
-  st.cache_data.clear()
+    st.cache_data.clear()
 
 def convert_pdf_to_text(file_path):
     text = ""
@@ -27,17 +28,21 @@ def main():
         data = []
         for file in uploaded_files:
             file_name = file.name
-            text = convert_pdf_to_text(file)
+            file_path = os.path.join("uploads", file_name)  # Save file locally
+            with open(file_path, "wb") as f:
+                f.write(file.getbuffer())
+            text = convert_pdf_to_text(file_path)
             data.append({"File Name": file_name, "Text": text})
+            os.remove(file_path)  # Remove the file after processing
 
         df = pd.DataFrame(data)
         st.subheader("Extracted Text")
         st.dataframe(df)
-        
+
 st.title("PDF to Text Converter")
 st.header("Upload PDF Files")
 
-uploaded_files = st.file_uploader("Choose your a file", type=['pdf'], on_change=reset_data, accept_multiple_files=True)
+uploaded_files = st.file_uploader("Choose your file", type=['pdf'], on_change=reset_data, accept_multiple_files=True)
 
 if uploaded_files:
-        main()
+    main()
