c84afdd0f48ef935c6a72c5bd3c016471cf53842
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 12:03:14 +0700

    Update 2 Journal.py

--- pages/2 Journal.py
+++ pages/2 Journal.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pdfplumber
 
-def extract_data(feed):
-    data = []
-    with pdfplumber.open(feed) as pdf:
-        pages = pdf.pages
-        for p in pages:
-            data.append(p.extract_tables())
-    return None # build more code to return a dataframe 
+def main(uploaded_file):
+    st.title("PDF to Text Converter")
 
-uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+    if uploaded_file is not None:
+        with pdfplumber.load(uploaded_file) as pdf:
+            text = ""
+            for page in pdf.pages:
+                text += page.extract_text()
+
+        st.subheader("Extracted Text")
+        st.text_area("Text", value=text, height=400)
+
+def reset_data():
+    st.cache_data.clear()
+
+st.header("Upload PDF File")
+uploaded_file = st.file_uploader("Choose a file", type=['pdf'], on_change=reset_data)
+
 if uploaded_file is not None:
-    df = extract_data(uploaded_file)
-    st.dataframe(df)
+    main(uploaded_file)
