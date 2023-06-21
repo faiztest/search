c54965c64bce1de3b56f1831cfcb03132099a7ff
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 11:54:44 +0700

    Update 2 Journal.py

--- pages/2 Journal.py
+++ pages/2 Journal.py
@@ -1,10 +1,5 @@
 import streamlit as st
-#===config===
-st.set_page_config(
-     page_title="Pencarian Terpadu",
-     page_icon="https://digilib.polteknuklir.ac.id/perpus/images/default/logo.png",
-     layout="wide"
-)
-st.write('hello there')
-uploaded_files = st.file_uploader("Choose your a file", type=['pdf'], accept_multiple_files=True)
-uploaded_file = st.file_uploader("Choose a file")
+
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+if uploaded_file is not None:
+    df = extract_data(uploaded_file)
