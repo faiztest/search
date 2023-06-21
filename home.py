66b72ced58fc7e056e60cdb1c82fcbc1346973f4
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 12:44:26 +0700

    Update home.py

--- home.py
+++ home.py
@@ -38,7 +38,7 @@ image_dict = {
 st.title('Search4All: Recorded materials')
 
 #Sidebar (menu)
-st.sidebar.success("Choose menu")
+#st.sidebar.success("Choose menu")
 
 # Intro text
 st.caption(f"Discover and learn among the more than **{df.shape[0]}** sources available from Search4All.")
