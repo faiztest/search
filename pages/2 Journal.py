28592fbc1ee8ded3922aa6b006739ee38e92ec61
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 11:36:50 +0700

    Update 2 Journal.py

--- pages/2 Journal.py
+++ pages/2 Journal.py
@@ -5,6 +5,6 @@ st.set_page_config(
      page_icon="https://digilib.polteknuklir.ac.id/perpus/images/default/logo.png",
      layout="wide"
 )
-st.write('hello')
+st.write('hello there')
 uploaded_files = st.file_uploader("Choose your a file", type=['pdf'], accept_multiple_files=True)
 uploaded_file = st.file_uploader("Choose a file")
