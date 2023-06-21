2b931d98ce8d5654bd0bb03c11bf39dbf4359384
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 12:06:13 +0700

    Update 2 Journal.py

--- pages/2 Journal.py
+++ pages/2 Journal.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pdfplumber
 
+def cache_clear():
+    st.cache_data.clear()
+
+@st.cache_data
 def main(uploaded_file):
     st.title("PDF to Text Converter")
 
@@ -13,11 +17,8 @@ def main(uploaded_file):
         st.subheader("Extracted Text")
         st.text_area("Text", value=text, height=400)
 
-def reset_data():
-    st.cache_data.clear()
-
 st.header("Upload PDF File")
-uploaded_file = st.file_uploader("Choose a file", type=['pdf'], on_change=reset_data)
+uploaded_file = st.file_uploader("Choose a file", type=['pdf'], on_change=cache_clear)
 
 if uploaded_file is not None:
     main(uploaded_file)
