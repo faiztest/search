7d74045cf0fcc5c08672714334feb9324b014d7a
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 11:56:26 +0700

    Update 2 Journal.py

--- pages/2 Journal.py
+++ pages/2 Journal.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pdfplumber
+
+def extract_data(feed):
+    data = []
+    with pdfplumber.load(feed) as pdf:
+        pages = pdf.pages
+        for p in pages:
+            data.append(p.extract_tables())
+    return None # build more code to return a dataframe 
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
