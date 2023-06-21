f779ef3f11fd5f067f899f090176aeb5bf534637
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-21 12:12:50 +0700

    Update 2 Journal.py

--- pages/2 Journal.py
+++ pages/2 Journal.py
@@ -6,11 +6,15 @@ def cache_clear():
 
 @st.cache_data
 def main(uploaded_file):
+
     if uploaded_file is not None:
-        with pdfplumber.open(uploaded_file) as pdf:
+        with pdfplumber.load(uploaded_file) as pdf:
             text = ""
             for page in pdf.pages:
                 text += page.extract_text()
+
+        st.subheader("Extracted Text")
+        st.text_area("Text", value=text, height=400)
 
 
 st.title("PDF to Text Converter")
@@ -18,6 +22,7 @@ st.header("Upload PDF File")
 uploaded_file = st.file_uploader("Choose a file", type=['pdf'], on_change=cache_clear)
 
 if uploaded_file is not None:
-    text = main(uploaded_file)
     st.subheader("Extracted Text")
-    st.text_area("Text", value=text, height=400)
+    main(uploaded_file)
+    
+
