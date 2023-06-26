34230fb2a57e5d3a0f3bd5ca033cf6e7f8ecbd07
faiztest <129721483+faiztest@users.noreply.github.com>
2023-06-26 07:39:17 +0700

    Update 1 pdf to text.py

--- pages/1 pdf to text.py
+++ pages/1 pdf to text.py
@@ -1,29 +1,66 @@
 import streamlit as st
 import pdfplumber
+import pandas as pd
+import streamlit.components.v1 as components
+import sys
 
-def cache_clear():
-    st.cache_data.clear()
+st.set_page_config(
+     page_title="PDFs to dataframe",
+     page_icon="https://github.com/faizhalas/Search4All/blob/main/images/logo.png?raw=true",
+     layout="wide"
+)
 
-@st.cache_data(experimental_allow_widgets=True)
+def clear_data():
+     st.cache_data.clear()
+
+@st.cache_data(ttl=3600, experimental_allow_widgets=True)
 def convert(uploaded_files):
-    results = []
+    data = []
     for file in uploaded_files:
         with pdfplumber.open(file) as pdf:
             text = ""
             for page in pdf.pages:
                 text += page.extract_text()
-        results.append(text)
-    return results
+        data.append({"File Name": file.name, "Text": text})
+    df = pd.DataFrame(data).replace(r'\n',' ', regex=True)
+    return df
+
+@st.cache_data(ttl=3600)
+def remove_before(df):
+    if rmv:
+         df['Text'] = df['Text'].str.split(rmv).str[-1].str.strip()
+    return df
+
+
+@st.cache_data(ttl=3600)
+def split(extracted_data):
+    pattern = '|'.join(word_list)
+    split_df = df['Text'].str.split(pattern, expand=True)
+    result_df = pd.concat([df, split_df], axis=1)
+    new_columns = ['File Name', 'Text', 'intro'] + word_list
+    result_df.columns = new_columns
+    return result_df    
+
 
 st.title("PDF to Text Converter")
 st.header("Upload PDF Files")
 
-uploaded_files = st.file_uploader("Choose files", type=['pdf'], accept_multiple_files=True, on_change=cache_clear)
+uploaded_files = st.file_uploader("Choose files", type=['pdf'], accept_multiple_files=True)
 
-if uploaded_files is not None:
-    st.subheader("Extracted Text")
-    extracted_texts = convert(uploaded_files)
-    for i, text in enumerate(extracted_texts):
-        file_name = uploaded_files[i].name
-        st.subheader(file_name)
-        st.text_area(f"Text {i+1}", value=text, height=400)
+rmv = st.text_input("Remove certain text before 'your text'.")
+
+text_search = st.text_input("Split your PDFs into parts. Separate words (cAsE sEnSiTiVe) by semicolons (;)")
+word_list = [keyword.strip() for keyword in text_search.split(";")]
+
+if st.button("Convert", on_click=clear_data):
+    try:
+         df = convert(uploaded_files)
+         rdf = remove_before(df)
+         result_df = split(rdf)
+    except ValueError:
+            st.error('Error: Please double-check the words that are used as splitter.')
+            sys.exit(1)
+         
+    if not result_df.empty:
+        st.subheader("Extracted Text")
+        st.dataframe(result_df)
